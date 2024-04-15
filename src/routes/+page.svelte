6b84d0fb5bd19d44c45ceeddf642e6038f53542f
chedieck <estevao@lobo.dev.br>
2024-04-15 17:25:08 -0300

    fix: build and desc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,27 +6,26 @@
 	<!-- Primary Meta Tags -->
 	<title>Decision Race</title>
 	<meta charset="UTF-8"/>
-	<meta name="title" content="Decision Race - Make Choices Fun"/>
-	<meta name="description" content="Decision Race, the app where choices become a thrilling race! Perfect for settling decisions, picking options, or just for fun. Whether it's choosing tonight's movie, the next book club read, where to dine, or who gets the last cookie in the pack, Decision Race adds excitement to every choice. Visit decisionrace.com and let the race begin!"/>
+	<meta name="title" content="DecisionRace — Choosing made fun!"/>
+	<meta name="description" content="Decision Race, the app where choices become a thrilling race! Perfect for settling decisions, picking options, or just for fun. Whether it's choosing tonight's movie, the next book club read, where to dine, or who gets the last cookie in the pack, Decision Race adds excitement to every choice. Visit race.chedieck.com and let the race begin!"/>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 	<meta name="keywords" content="Decision Making, Choices, Fun, Race, Entertainment, Interactive, Games, Decision Help, Choice Assistance"/>
 	<meta name="author" content="Estevão Chedieck Lobo"/>
 	<meta name="language" content="English"/>
-	<link rel="canonical" href="https://www.decisionrace.com"/>
+	<link rel="canonical" href="https://race.chedieck.com"/>
 	<!-- Social Media Tags -->
 
 	<meta property="og:type" content="website"/>
-	<meta property="og:url" content="https://www.decisionrace.com"/>
-	<meta property="og:title" content="Decision Race - Make Choices Fun"/>
-	<meta property="og:description" content="Decision Race, the app where choices become a thrilling race! Perfect for settling decisions, picking options, or just for fun. Visit decisionrace.com and let the race begin!"/>
-	<meta property="og:image" content="https://www.decisionrace.com/1200x630-facebook.png"/>
+	<meta property="og:url" content="https://race.chedieck.com"/>
+	<meta property="og:title" content="DecisionRace — Choosing made fun!"/>
+	<meta property="og:description" content="Decision Race, the app where choices become a thrilling race! Perfect for settling decisions, picking options, or just for fun. Visit race.chedieck.com and let the race begin!"/>
+	<meta property="og:image" content="https://race.chedieck.com/1200x630-facebook.png"/>
 	<!-- Twitter Card data -->
 	<meta name="twitter:card" content="summary_large_image"/>
-	<meta name="twitter:title" content="Decision Race - Make Choices Fun"/>
-	<meta name="twitter:description" content="Decision Race, the app where choices become a thrilling race! Perfect for settling decisions, picking options, or just for fun. Visit decisionrace.com and let the race begin!"/>
-	<meta name="twitter:image" content="U/1200x628-twitter.png"/>
+	<meta name="twitter:title" content="DecisionRace — Choosing made fun!"/>
+	<meta name="twitter:description" content="Decision Race, the app where choices become a thrilling race! Perfect for settling decisions, picking options, or just for fun. Visit race.chedieck.com and let the race begin!"/>
 
 
 </svelte:head>
